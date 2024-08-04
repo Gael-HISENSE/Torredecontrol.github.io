a1229fdb2f2163ebcba2213f86764cfa5acdeb93
Gael-HISENSE <gael.gruposid@gmail.com>
2024-08-03 18:44:40 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// Aquí va todo el código del componente React que te proporcioné anteriormente
+// Asegúrate de copiar todo el código del componente LogisticsControlTower aquí
+
+ReactDOM.render(<LogisticsControlTower />, document.getElementById('root'));
